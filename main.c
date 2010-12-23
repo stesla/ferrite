c7d97bda301eadd7bf693aba33321e2676ba935b
Samuel Tesla <samuel.tesla@gmail.com>
2010-12-22 23:50:39 -0600

    read from a file

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include "eval.h"
 #include "gc.h"
@@ -7,9 +11,26 @@
 #include "vm.h"
 
 int main(int argc, char **argv) {
-  fe_read(STDIN_FILENO);
+  int input_fd;
+
+  if (argc != 2) {
+    fprintf(stderr, "USAGE: %s file\n", argv[0]);
+    exit(1);
+  }
+
+  if (strcmp("-", argv[1]) == 0)
+    input_fd = STDIN_FILENO;
+  else {
+    input_fd = open(argv[1], O_RDONLY);
+    if (input_fd < 0) {
+      perror("could not open file");
+      exit(1);
+    }
+  }
+
+  fe_read(input_fd);
   vm.e = vm_pop_s();
-  fe_read(STDIN_FILENO);
+  fe_read(input_fd);
   fe_eval();
   return 0;
 }
--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 for test in ${@:-*.test.crx}; do
     printf "%s..." $(basename $test .test.crx)
     expected=$(sed -e '1,/RESULT/d' -e '/^$/d'< $test)
-    actual=$(sed -e '/RESULT/,$d' -e '/^$/d' < $test | ${program:=$PWD/fe -s} 2>&1)
+    actual=$(sed -e '/RESULT/,$d' -e '/^$/d' < $test | ${program:=$PWD/fe -} 2>&1)
     if [ $? -ne 0 ]; then
         status=1
         echo ERROR
